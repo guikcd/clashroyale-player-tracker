15224625f2e7f8f495c6e45d9569bcc2b8d2536c
Guillaume Delacour <gui@iroqwa.org>
2018-10-21 12:25:12 +0200

    s/CLAN_TAG/PLAYER_TAG

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker build -t player-gui:0.1 .
 bash ./tests.sh
 docker stop player-gui && docker rm -f player-gui
-docker run --restart always --env CR_API_TOKEN=$(cat token) --env INFLUXDB_HOST=influxdb -e INFLUXDB_PORT=8086 -e INFLUXDB_LOGIN=root -e INFLUXDB_PASSWORD=root -e INFLUXDB_DATABASE=clashroyale_stats -e CLAN_TAG=2G00222J --link influxdb --name player-gui --detach player-gui:0.1
+docker run --restart always --env CR_API_TOKEN=$(cat token) --env INFLUXDB_HOST=influxdb -e INFLUXDB_PORT=8086 -e INFLUXDB_LOGIN=root -e INFLUXDB_PASSWORD=root -e INFLUXDB_DATABASE=clashroyale_stats -e PLAYER_TAG=2G00222J --link influxdb --name player-gui --detach player-gui:0.1
 docker logs player-gui
